05eae88410ab9349e2ffdb2ad022070c1bcf1f54
PriyaSundaram24 <116790518+PriyaSundaram24@users.noreply.github.com>
2022-12-14 05:43:02 +0000

    updated the pprint library

--- My Programs/PSlogin.py
+++ My Programs/PSlogin.py
@@ -1,7 +1,13 @@
 import pandas as pd
-import simple_salesforce as sforce
+from pprint import pprint
 from simple_salesforce import Salesforce
 sf = Salesforce(password='Trailhead@123', username='priyasundaram@industries.com', security_token='RYEOwQJ8jq9wQJU9ly7lV76S',organizationId='00D5g00000JRSGo')
 data = sf.query("SELECT Id, Name FROM Contact")
 sf_df = pd.DataFrame(data['records']).drop(columns='attributes')
 print(sf_df)
+
+SOQL = "SELECT Id, Email FROM Contact"
+data = sf.query(SOQL)
+print(data)
+for d in data['records']:
+    pprint(f"{d['Id']} -- {d['Email']}")
